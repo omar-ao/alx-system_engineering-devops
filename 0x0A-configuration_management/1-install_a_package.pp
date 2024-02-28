ab490bebb264410dbc1d3ce0b128e78a55c6dd5d
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-28 12:10:05 +0300

    Add task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
-# Installs flask from pip3
+# Installs pip3
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package { 'flask':
+# Install flask=2.1.0
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+# Install wrkseug 2.2.1
+package { 'Werkzeug':
+  ensure   => '2.2.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
