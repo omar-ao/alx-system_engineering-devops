b2a366725301d7399958105457e007bf342e2388
Omar Ahmed O <deeyndiid@gmail.com>
2024-03-05 22:09:56 +0300

    Update task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,35 +1,16 @@
-# Configure new machine
-class { 'nginx':
-  package_ensure => 'latest',
-}
+# This script automates the task of creating a custom HTTP header response, with Puppet.
 
-file { '/var/www/html/index.nginx-debian.html':
-  ensure  => present,
-  content => 'Hello World!',
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-nginx::resource::vhost { 'default':
-  www_root       => '/usr/share/nginx/html',
-  error_page     => '404 /404.html',
-  error_page_dir => '/usr/share/nginx/html',
-  add_header     => {
-    'X-Served-By' => '$HOSTNAME',
-  },
-  locations      => {
-    '/redirect_me' => {
-      ensure   => present,
-      content  => 'return 301 http://youtube.com;',
-    },
-  },
+-> package {'nginx':
+  ensure => 'present',
 }
-
-file { '/usr/share/nginx/html/404.html':
-  ensure  => present,
-  content => "Ceci n'est pas une page\n",
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => File['/var/www/html/index.nginx-debian.html'],
+-> exec {'restart':
+  command => '/usr/sbin/service nginx restart',
 }
