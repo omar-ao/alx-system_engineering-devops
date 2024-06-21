434e49d3211c6aeab8940b0eb782d96c8e9e2c2a
Omar Ahmed O <deeyndiid@gmail.com>
2024-06-21 05:51:23 +0300

    Update task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""This is 2-recurse module"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=''):
+    """
+        queries the Reddit API and returns a list containing
+        the titles of all hot articles for a given subreddit.
+    """
+
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
+                                                                 after)
+    headers = {"User-Agent": "Python script (by/Omar"}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return None
+    posts = r.json().get('data')
+    for post in posts.get('children'):
+        hot_list.append(post.get('data').get('title'))
+    after = posts.get('after')
+    if after:
+        recurse(subreddit, hot_list, after)
+    return hot_list
